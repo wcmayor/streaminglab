813977735ae583c18e834642146f358109224b19
Cameron Mayor <wcmayor@gmail.com>
2018-10-25 21:08:13 -0400

    initial commit of td-agent cred download script

---
+++ update_td-agent_creds.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import json
+import boto3
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description="Parser")
+parser.add_argument("secret_name", type=str)
+parser.add_argument("region", type=str)
+
+args=parser.parse_args()
+
+session = boto3.session.Session()
+client = session.client(service_name='secretsmanager', region_name=args.region)
+
+secrets = json.loads(client.get_secret_value(SecretId=args.secret_name)["SecretString"])
+
+f = open("/home/td-agent/.bashrc", "a")
+
+for key, value in secrets.items():
+    if key == "sqs_url":
+        f.write('export td-agent_sqs_queue="' + value + '"')
+    if key == "sqs_publish_key":
+        f.write('export td-agent_sqs_key="' + value + '"')
+    if key == "sqs_publish_secret_key":
+        f.write('export td-agent_sqs_secret_key="' + value + '"')
